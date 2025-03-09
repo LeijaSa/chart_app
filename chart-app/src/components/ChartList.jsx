78c1f2693cadb3ad879468a88f6fbd6111f8cdf0
Leija Salmivirta <leija.salmivirta@gmail.com>
2025-03-09 19:02:20 +0200

    Added menu box for editing and deleting charts

--- chart-app/src/components/ChartList.jsx
+++ chart-app/src/components/ChartList.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setSelectedChart } from "../redux/chartSlice";
-import { List, ListItem, ListItemText } from "@mui/material";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Link } from "react-router-dom";
 
 const ChartList = ({ searchTerm }) => {
@@ -11,6 +19,29 @@ const ChartList = ({ searchTerm }) => {
   const charts = useSelector((state) => state.charts.charts);
   const selectedChart = useSelector((state) => state.charts.selectedChart);
 
+  const [anchorEl, setAnchorEl] = useState(null); // State to open/close the menu
+  const [selectedChartId, setSelectedChartId] = useState(null); // Track which chart the options are for
+
+  const handleMenuClick = (event, chartId) => {
+    setAnchorEl(event.currentTarget);
+    //setSelectedChartId(chartId); // Set the selected chart ID for actions
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleEdit = () => {
+    console.log("Edit chart with ID:", selectedChartId);
+    // Handle the edit logic here
+    handleMenuClose();
+  };
+
+  const handleDelete = () => {
+    console.log("Delete chart with ID:", selectedChartId);
+    // Handle the delete logic here
+    handleMenuClose();
+  };
+
   // If searchTerm is null or empty, show all charts
   const filteredCharts = searchTerm
     ? charts.filter((chart) =>
@@ -19,6 +50,7 @@ const ChartList = ({ searchTerm }) => {
     : charts; // Return all charts if no search term
 
   const handleChartClick = (chart) => {
+    console.log("chart clicked!!");
     dispatch(setSelectedChart(chart));
   };
 
@@ -46,6 +78,32 @@ const ChartList = ({ searchTerm }) => {
           >
             <ListItemText primary={chart.name} />
           </Link>
+
+          {/* Three-dot icon to open the menu */}
+          <IconButton
+            edge="end"
+            aria-label="more"
+            onClick={(event) => handleMenuClick(event, chart.chartId)}
+          >
+            <MoreVertIcon />
+          </IconButton>
+          {/* Menu for Edit and Delete actions */}
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)} // Menu opens only if anchorEl is set
+            onClose={handleMenuClose} // Closes when clicking outside
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+          >
+            <MenuItem onClick={handleEdit}>Edit</MenuItem>
+            <MenuItem onClick={handleDelete}>Delete</MenuItem>
+          </Menu>
         </ListItem>
       ))}
     </List>
